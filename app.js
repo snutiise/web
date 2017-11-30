96f3e83ab025a86d4abc97483d4be3a43415a0b2
snutiise <snutiise@hanmail.net>
2017-11-30 16:33:05 +0900

    수정본1

--- app.js
+++ app.js
@@ -8,27 +8,24 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended : true}));
 
-app.post('/Gallery', function(req, res){
-    var cnt = Number(req.body.count);
-    var list = new Array();
-    var process=0;
+app.get('/', function(req,res){
+    res.redirect('http://sodeok.xyz');
+});
+
+app.get('/cnt', function(req, res, next) {
+    res.redirect('/index.html');
+});
+
+app.post('/count', function(req, res){
+    var gte = Number(req.body.gte);
+    var lt = Number(req.body.lt);
 
     Client.connect('mongodb://localhost:27017/yagall', function(error, db) {
         if(error) console.log(error);
         else {
-            var cursor = db.collection('img').find().sort({num:-1}).skip(cnt).limit(30);
-            var stop=0;
-            db.collection('img').find().sort({num:-1}).skip(cnt).limit(30).count(function(err, cnt){
+            db.collection('drop').find({num:{'$gte':gte,'$lt':lt}}).count(function(err, cnt){
                 if(err) console.log(err);
-                stop=Number(cnt);
-            });
-            cursor.each(function(err, doc){
-                if(err) console.log(err);
-                else{
-                        process++;
-                        if(doc!=null) list.push(doc['_id']);
-                        if(process==stop) res.send(list);
-                    }
+                res.send(Number(cnt));
             });
             db.close();
         }
